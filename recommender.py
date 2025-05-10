86a4ee09058389e7ba4edc9d5cbc06e6f7aa9af7
iamshubhamk <shubham.kr.735@gmail.com>
2025-05-11 03:42:51 +0530

    Refactor recommender script to use environment variable for SERPAPI key and update test run query

--- recommender.py
+++ recommender.py
@@ -5,13 +5,13 @@ import os
 # 1. Initialize Ollama LLM (running locally)
 llm = Ollama(model="gemma3")
 
-# SERPAPI_KEY = os.getenv("SERPAPI_KEY")
+SERPAPI_API_KEY = os.getenv("SERPAPI_KEY")
 
 # 2. Load serpapi tool with your API key
-tools = load_tools(["serpapi", "wikipedia"], serpapi_api_key="4b626322eb043351a70c538ace27ed724aa991a5b8c4b187eb8b8e867ef07a51")
+tools = load_tools(["serpapi", "wikipedia"], serpapi_api_key=SERPAPI_API_KEY)
 
 # 3. Initialize agent
 agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
 # 4. Test run
-agent.run("Shah Rukh Khan")
+agent.run("recommend me gaming laptops under 55000 rupees along with the links to buy them")
